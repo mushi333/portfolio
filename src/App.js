0b5041faacbf95cc498705ac3ab1152f4cdc4dd1
mushi333 <mujtaba.zahidi24@gmail.com>
2021-08-02 18:42:25 +1000

    Created basic footer.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import MainNavbar from "./component/navbar/MainNavbar.js";
+import MainFooter  from './component/footer/MainFooter';
 
 function App() {
   return (
-    <Container className="Navigation">
+    <Container className="Main">
       <MainNavbar />
+      <MainFooter />
     </Container>
-
   );
 }
 
--- src/component/navbar/MainNavbar.js
+++ src/component/navbar/MainNavbar.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 class MainNavbar extends React.Component {
     render() {
